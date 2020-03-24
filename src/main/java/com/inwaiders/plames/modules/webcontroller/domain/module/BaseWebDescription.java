6b796a65226d3554e59a58410578b3f2e5693526
Redn_oll <rednoll12@gmail.com>
2020-03-24 19:27:06 +0300

    test

--- src/main/java/com/inwaiders/plames/modules/webcontroller/domain/module/BaseWebDescription.java
+++ src/main/java/com/inwaiders/plames/modules/webcontroller/domain/module/BaseWebDescription.java
@@ -3,15 +3,19 @@ package com.inwaiders.plames.modules.webcontroller.domain.module;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.inwaiders.plames.domain.module.impl.ModuleBase;
 import com.inwaiders.plames.modules.webcontroller.domain.module.button.Button;
 
-public abstract class WebDescribedModuleBase extends ModuleBase implements WebDescribedModule {
+public class BaseWebDescription implements WebDescription {
 
 	protected List<Button> buttons = new ArrayList<>();
 	
-	public WebDescribedModuleBase() {
+	public BaseWebDescription() {
 		
+	}
+	
+	public void addButton(Button button) {
+		
+		this.buttons.add(button);
 	}
 	
 	@Override
--- src/main/java/com/inwaiders/plames/modules/webcontroller/domain/module/WebDescribedModule.java
+++ src/main/java/com/inwaiders/plames/modules/webcontroller/domain/module/WebDescribedModule.java
@@ -1,10 +1,6 @@
 package com.inwaiders.plames.modules.webcontroller.domain.module;
 
-import java.util.List;
-
-import com.inwaiders.plames.modules.webcontroller.domain.module.button.Button;
-
 public interface WebDescribedModule {
 
-	public List<Button> getButtons();
+	public WebDescription getWebDescription();
 }
